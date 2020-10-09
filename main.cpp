f0b5a6c270eb6bb19fbb269793765f7efa5f9aa7
Michelle Bravo <bravomichelle292@gmail.com>
2020-10-08 22:21:00 -0400

    Add existing file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
+/*
+Author: Michelle Bravo
+Course: CSCI-136
+Instructor: Mike Zamansky
+Assignment: Lab 06
+
+Purpose: call void test_ascii() from main
+*/
+
 #include <iostream>
-
-
 #include "funcs.h"
+
 
 int main()
 {
+	using std::string;
+	using std::cout;
+	using std::endl;
+
+	test_ascii("Cat :3 Dog");
+	cout<<"\n";
+
+	test_ascii("50");//evaluates to 53-48
+	cout<<"\n";
+
+	test_ascii("Hello world");
+	cout << "\n";
+
+	//encryptCaesar("Hello World", 10);
+	//cout << "Ciphertext: ";
+	//cout << "\n";
+
   return 0;
 }
