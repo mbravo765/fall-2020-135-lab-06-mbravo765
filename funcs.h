faa0fa893dcf35e7ef08e79db68959bf133add46
Michelle Bravo <bravomichelle292@gmail.com>
2020-10-08 22:19:40 -0400

    Add existing file

--- funcs.h
+++ funcs.h
@@ -1,3 +1,13 @@
-#pragma once
+/*
+Author: Michelle Bravo
+Course: CSCI-136
+Instructor: Mike Zamansky
+Assignment: Lab 06
 
+*/
+
+#pragma once
+#include <string>
 // add prototypes here
+void test_ascii(std::string line);
+
