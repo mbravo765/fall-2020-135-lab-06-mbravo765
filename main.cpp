2a378f2c7a61899dad641a31575a7d8f646f02de
Michelle Bravo <bravomichelle292@gmail.com>
2020-10-09 05:49:46 -0400

    Add file

--- main.cpp
+++ main.cpp
@@ -9,13 +9,12 @@ Purpose: call void test_ascii() from main
 
 #include <iostream>
 #include "funcs.h"
-
-
 int main()
 {
 	using std::string;
 	using std::cout;
 	using std::endl;
+	using std::cin;
 
 	test_ascii("Cat :3 Dog");
 	cout<<"\n";
@@ -26,9 +25,6 @@ int main()
 	test_ascii("Hello world");
 	cout << "\n";
 
-	//encryptCaesar("Hello World", 10);
-	//cout << "Ciphertext: ";
-	//cout << "\n";
 
   return 0;
 }
